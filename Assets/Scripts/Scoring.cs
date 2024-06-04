1f8b2350147d7aae79c8260866d379f88f9c5012
DPS-2005 <61118074+DPS-2005@users.noreply.github.com>
2024-06-04 20:38:19 +0530

    temporary pause and gameover functionality

--- Assets/Scripts/Scoring.cs
+++ Assets/Scripts/Scoring.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Scoring : MonoBehaviour
 {
@@ -9,11 +11,47 @@ public class Scoring : MonoBehaviour
         if(collision.collider.tag == "Obstacle" || collision.collider.tag == "Player")
         {
             //do some effects
-            GameManager.Instance.GameOver();
+            GameOver();
         }
         if(collision.collider.tag == "Food")
         {
             GameManager.Instance.IncreaseScore();
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        GameManager.Instance.paused = true;
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        GameManager.Instance.paused = false;
+        GameManager.Instance.canvas.transform.GetChild(0).gameObject.SetActive(false);
+    }
+
+    public void GameOver()
+    {
+        Pause();
+        GameManager.Instance.canvas.transform.GetChild(1).gameObject.SetActive(true);
+    }
+
+    public void TryAgain()
+    {
+        Time.timeScale = 1;
+        GameManager.Instance.paused = false;
+        GameManager.Instance.canvas.transform.GetChild(1).gameObject.SetActive(false);
+        StartCoroutine(GameManager.Instance.LoadSceneAsync());
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !GameManager.Instance.paused)
+        {
+            Pause();
+            GameManager.Instance.canvas.transform.GetChild(0).gameObject.SetActive(true);
         }
     }
 }
